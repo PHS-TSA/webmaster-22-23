a21c39f672a22b42c7e7e18ab9dcaf10240301cf
lishaduck <88557639+lishaduck@users.noreply.github.com>
2022-12-21 11:20:09 -0600

    fix sync

--- public/importmap.js
+++ public/importmap.js
@@ -1,8 +1,10 @@
-document.write(`<script type="importmap">
+var container = document.appendChild(
+  `<script type="importmap">
   {
     "imports": {
       "@popperjs/core": "https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js",
       "bootstrap": "https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.esm.min.js"
     }
   }
-</script>`);
+  </script>`
+);
