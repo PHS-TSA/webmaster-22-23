2a154b8ed2815257076b64bd03246911bd9b6f1c
lishaduck <88557639+lishaduck@users.noreply.github.com>
2022-12-21 14:14:57 -0600

    fix: innerHTML is a property, not a function

--- public/importmap.js
+++ public/importmap.js
@@ -1,11 +1,9 @@
 const container = document.getElementById("importmpa");
 const script = document.createElement("script");
 script.setAttribute("type", "importmap");
-script.innerHTML(
-  `{
+script.innerHTML = `{
     "imports": {
       "@popperjs/core": "https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js",
       "bootstrap": "https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.esm.min.js"
-    }`
-);
+    }`;
 var contain = container.appendChild(script);
