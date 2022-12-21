c4982d388bcfebbcc610a38a4250ebc159b0ec84
lishaduck <88557639+lishaduck@users.noreply.github.com>
2022-12-21 09:01:42 -0600

    fix: importmap

---
+++ public/importmap.js
@@ -0,0 +1,8 @@
+document.write(`<script type="importmap">
+  {
+    "imports": {
+      "@popperjs/core": "https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js",
+      "bootstrap": "https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.esm.min.js"
+    }
+  }
+</script>`);
