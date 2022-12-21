0dbd2689e6b22991624611ce91dc8ae9cf7f843a
lishaduck <88557639+lishaduck@users.noreply.github.com>
2022-12-21 13:47:16 -0600

    refactor: importmap

--- public/importmap.js
+++ public/importmap.js
@@ -1,9 +1,10 @@
-var contained = document.createElement(`<script type="importmap">
-  {
+const script = document.createElement("script");
+script.setAttribute("type", "importmap");
+script.innerHTML(
+  `{
     "imports": {
       "@popperjs/core": "https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js",
       "bootstrap": "https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.esm.min.js"
-    }
-  }
-  </script>`);
-var container = document.appendChild(contained);
+    }`
+);
+var container = document.appendChild(script);
