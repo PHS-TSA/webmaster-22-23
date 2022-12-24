e7c94def53161eb27b1a212a58a8583b92ac0f7c
Eli <88557639+lishaduck@users.noreply.github.com>
2022-12-24 19:21:36 +0000

    feat(testing): add a snapshot test

--- src/canvas.test.ts
+++ src/canvas.test.ts
@@ -1,10 +1,19 @@
+/* eslint-disable prettier/prettier */
 /* eslint-disable no-magic-numbers */
 import { getDistance } from "./canvas";
-import { it, expect } from "vitest";
+import { it, expect, describe } from "vitest";
 
-it("add", () => {
-  expect(getDistance(1, 1, 1, 1)).toBe(0);
-  expect(getDistance(1, 2, 1, 2)).toBe(0);
-  expect(getDistance(1, 1, 2, 1)).toBe(1);
-  expect(getDistance(-5, 1, -1, 1)).toBe(4);
+// All tests within this suite will be run in parallel
+describe.concurrent("suite", () => {
+  it("Get the distance of various points.", async () => {
+    expect(getDistance(1, 1, 1, 1)).toBe(0);
+    expect(getDistance(1, 2, 1, 2)).toBe(0);
+    expect(getDistance(1, 1, 2, 1)).toBe(1);
+    expect(getDistance(-5, 1, -1, 1)).toBe(4);
+  });
+
+  it.concurrent("Snapshot getDistance.", async () => {
+    const result = getDistance(0, 0, 0, 0);
+    expect(result).toMatchInlineSnapshot("0");
+  });
 });
